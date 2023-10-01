8fed4ae9fb2106c3b48384f59babc639b8f443e0
Aleksej_Sivko <108893186+Chikago13@users.noreply.github.com>
2023-10-01 19:18:47 +0300

    Update cryptanalysis.py

--- cryptanalysis.py
+++ cryptanalysis.py
@@ -1,28 +1,27 @@
 # https://www.tradingview.com/symbols/BTSUSDT/
 # https://python-tradingview-ta.readthedocs.io/en/latest/overview.html
 # https://tvdb.brianthe.dev/  TradingView List
-# https://mastergroosha.github.io/aiogram-3-guide/quickstart/#hello-world
-
+# # https://mastergroosha.github.io/aiogram-3-guide/quickstart/#hello-world
+# http://docs.ccxt.com
 
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types, Router
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters.command import Command
-from tradingview_ta import TA_Handler, Interval, Recommendation, Exchange
-
+from tradingview_ta import TA_Handler, Interval, Recommendation
+import ccxt
 
 API_TOKEN ='6531929248:AAFTh9wDMnmqpkYpvl-h5ttT-OgGHNM4VkA'
 
-# RECOMMENDATIONS = {
-#     Recommendation.buy: 'Покупай',
-#     Recommendation.strong_buy: 'Срочно_покупай',
-#     Recommendation.sell: 'Продавай',
-#     Recommendation.strong_sell: 'Срочно_продавай',
-#     Recommendation.neutral: 'Непонятная ситуация',
-#     Recommendation.error: 'Ошибка',
-# }
-
+RECOMMENDATIONS = {
+    Recommendation.buy: 'Покупай',
+    Recommendation.strong_buy: 'Срочно_покупай',
+    Recommendation.sell: 'Продавай',
+    Recommendation.strong_sell: 'Срочно_продавай',
+    Recommendation.neutral: 'Непонятная ситуация',
+    Recommendation.error: 'Ошибка',
+}
 
 
 
@@ -32,46 +31,50 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 # Диспетчер
 dp = Dispatcher(bpt = bot)
-# router = Router()
-# dp.include_router(router)
+exchange = ccxt.binance()
 
 
 # Хэндлер на команду /start
-@dp.message(Command("/start"))
+@dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.reply("Hello!")
-
-@dp.message(Command("/test"))
-async def test1(message: types.Message):
-    await message.reply('some text')
-    
-
-# @dp.message()
-# async def currency(message: types.Message):
-#     try:
-#         response = TA_Handler(
-#             symbol=message.text,                 #торговая пара
-#             # symbol='BTCUSDT',
-#             screener="crypto",
-#             exchange="BINANCE",
-#             interval=Interval.INTERVAL_5_MINUTES
-#         )
-
-#         result = response.get_analysis().summary
-#         await message.answer(result)
-
-#         analysis = result.get('RECOMMENDATION', Recommendation.error)
-#         await message.answer(RECOMMENDATIONS.get(analysis))
-
-#     except Exception:
-#         await message.answer('Некорректный ввод')
+    await message.answer("Hello!")
 
 
+
+def get_current_price(symbol):
+    try:
+        ticker = exchange.fetch_ticker(symbol)
+        return ticker['last']
+    except:
+        return None
+
+
+
+@dp.message()
+async def get_crypto_price(message: types.Message):
+    try:
+        symbol = message.text.upper()
+        price = get_current_price(symbol)
+        if price is not None:
+            await message.answer(f"Текущая цена {symbol} равна {price}")
+            response = TA_Handler(
+                symbol=symbol,
+                screener="crypto",
+                exchange="BINANCE",
+                interval=Interval.INTERVAL_5_MINUTES
+            )
+            result = response.get_analysis().summary
+            # await message.answer(result)
+            analysis = result.get('RECOMMENDATION', Recommendation.error)
+            await message.answer(RECOMMENDATIONS.get(analysis))
+    except:
+        await message.answer('Некорректный ввод')        
 
 
 # # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
